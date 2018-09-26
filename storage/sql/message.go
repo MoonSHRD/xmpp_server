33efd585da5cb8a4325cf0597b8786fde63f99dc
Korristo <nekit01031996@mail.ru>
2018-09-26 16:43:11 +0300

    Improvements

--- storage/sql/message.go
+++ storage/sql/message.go
@@ -4,14 +4,14 @@ import (
 	sq "github.com/Masterminds/squirrel"
 )
 
-func (s *Storage) WriteMsgToDB(recipient, sender, msg string, isOnline int) (int64, error) {
-	check, _ := (*Storage).UserExists(s, recipient)
+func (s *Storage) WriteMsgToDB(chat_id, sender, msg string, isOnline int) (int64, error) {
+	check, _ := (*Storage).UserExists(s, chat_id)
 	if check == false {
 		return 0, nil
 	}
 	q := sq.Insert("messages").
 		Columns("chat_id", "sender", "message", "created_at", "updated_at", "delivered").
-		Values(recipient, sender, msg, sq.Expr("NOW()"), sq.Expr("NOW()"), isOnline)
+		Values(chat_id, sender, msg, sq.Expr("NOW()"), sq.Expr("NOW()"), isOnline)
 		res, err := q.RunWith(s.db).Exec()
 		if err != nil {
 			return 0, err
